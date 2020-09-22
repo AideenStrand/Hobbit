3547315aa6e2a63cd35c45cd989f25ac66aedcfe
Aideen Strandsson <aideen.strandsson@knowit.se>
2020-09-22 15:47:03 +0200

    added changes

---
+++ src/main/java/project/data/TimeRegister.java
@@ -0,0 +1,7 @@
+package project.data;
+
+import java.util.Date;
+
+public interface TimeRegister {
+   Boolean fixBirthDate(String date);
+}
--- src/main/java/project/service/ClientService.java
+++ src/main/java/project/service/ClientService.java
@@ -3,35 +3,51 @@ package project.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import project.client.ClientServiceHttp;
 import project.data.CostumerInformation;
 import project.data.Petstore;
 import project.data.ResponseJson;
+import project.data.TimeRegister;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @Slf4j
 @Service
-public class ClientService {
+public class ClientService implements TimeRegister {
 
     @Autowired
     private ClientServiceHttp galacServiceClient;
 
+    private final static String BIRTHDATE = "12/03/1980";
+
     public ResponseJson getAip(String status) {
         ResponseJson responseJson = new ResponseJson();
         List<Petstore> petstore = galacServiceClient.getResponseFromDatabase(status);
+        String name = petstore.stream().map(c -> c.getName()).findFirst().orElse(null);
+        if (fixBirthDate(BIRTHDATE)) {
+            CostumerInformation costumerInformation = new CostumerInformation.MyBuilder()
+                    .name(name)
+                    .family("berger")
+                    .personalId("12")
+                    .myBuild();
+            responseJson.setCompleteName(costumerInformation.getName() + "  " + costumerInformation.getFamily());
+            responseJson.setCostumerInformation(costumerInformation);
 
-        String name = petstore.stream().map(c->c.getName()).findFirst().orElse(null);
-
-        CostumerInformation costumerInformation = new CostumerInformation.MyBuilder()
-                .name(name)
-                .family("berger")
-                .personalId("12")
-                .myBuild();
-
-        responseJson.setCompleteName(costumerInformation.getName() + "  " + costumerInformation.getFamily());
-        responseJson.setCostumerInformation(costumerInformation);
+        }
         return responseJson;
     }
 
+    @Override
+    public Boolean fixBirthDate(String date) {
+        Date today = new Date();
+        SimpleDateFormat DateFor = new SimpleDateFormat("dd/MM/yyyy");
+        String stringDate = DateFor.format(today);
+        if (BIRTHDATE.equals(stringDate)) {
+            return false;
+        }
+        return true;
+    }
 }
