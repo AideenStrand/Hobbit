55b4fb7580b7c2684232f84427ef35acc17772e8
aideenStrKnowit <aideen.strandsson@gmail.com>
2020-10-15 18:05:26 +0200

    added some changes

--- src/main/java/project/service/ClientService.java
+++ src/main/java/project/service/ClientService.java
@@ -4,7 +4,6 @@ import com.github.fge.jsonpatch.JsonPatch;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import project.client.ClientServiceHttp;
 import project.data.CostumerInformation;
